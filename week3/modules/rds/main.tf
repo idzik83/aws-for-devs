8a3e0d2bfc7436fc93ab2b4edde72335f46c463a
Igor Idzikovskyi <igor.idzikovskyi@agari.com>
2021-04-16 12:05:58 +0300

    ad SG to RDS

--- week3/modules/rds/main.tf
+++ week3/modules/rds/main.tf
@@ -6,5 +6,15 @@ resource "aws_db_instance" "aws_4_dev_rds" {
   username             = var.rds_username
   password             = var.rds_user_pass
   skip_final_snapshot  = true
-  security_group_names = []
+  security_group_names = [aws_security_group.ingress_rds.name]
+}
+
+resource "aws_security_group" "ingress_rds" {
+  name = "ingress_rds"
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
